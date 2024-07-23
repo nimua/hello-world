c2eb5b7ea008bd0af5d9cdc09a871bbb9486fa0c
like <52870888+nimua@users.noreply.github.com>
2024-07-23 16:11:54 +0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,54 +1,90 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-mapping assembly and polish
+using udance to make a tree 
 """
 
 import subprocess
 import os
+import yaml
+
 
 def execute_command_in_conda_env(conda_env, command):
     full_command = f'conda run -n {conda_env} {command}'
     subprocess.run(full_command, shell=True)
+
+
+def get_abs_path(path, current_path):
+    if os.path.isabs(path):
+        return path
+    else:
+        return os.path.join(current_path, path)
 
 if __name__=="__main__":
     import sys
     import argparse
 
     example_text = '''example:
-        mapping_assembly.py -p 4 -r single_fasta -i test_data.fasta -o genome.fasta --medaka r941_min_high_g303 --homopolish R9.4.pkl 
+        make_tree.py -p 4 -f single_fasta -o tree --udance ./uDance
         '''
 
-    parser = argparse.ArgumentParser(prog='mapping_assembly.py',
+    parser = argparse.ArgumentParser(prog='make_tree.py',
                                      description='Run bash cmd lines for files',
                                      epilog=example_text,
                                      formatter_class=argparse.RawDescriptionHelpFormatter)
-
-
+    
+    
     parser.add_argument("-p", "--processes", help="number of processes", default= 4)
-    parser.add_argument("-i", "--input", help="input fasta or fastq file")
-    parser.add_argument("-r", "--ref", help="a folder containing multiple ASFV genomes, or a single reference sequence file")
-    parser.add_argument("-o", "--output", help="assembled ASFV genome")
-    parser.add_argument("--medaka", help="medaka model",default=None)
-    parser.add_argument("--homopolish", help="homopolish model",default=None)
+    parser.add_argument("-f", "--file", help="dir of the fasta files")
+    parser.add_argument("-o", "--output", help="output dir")
+    parser.add_argument("--udance", help="path to udance dir")
 
     args = parser.parse_args(args=None if sys.argv[1:] else ['--help'])
     num_processes = args.processes
-    input_reads = args.input
-    ref = args.ref
-    output = args.output
-    medaka_model = args.medaka
-    homopolish_model = args.homopolish
+    input_folder = args.file
+    output_folder = args.output
+    udance_folder = args.udance
+
+    current_path = os.getcwd()
+    output_folder = get_abs_path(output_folder, current_path)
+    input_folder = get_abs_path(input_folder, current_path)
+    udance_folder = get_abs_path(udance_folder, current_path)
+
+    try:
+        os.makedirs(output_folder)
+    except FileExistsError:
+        pass
+    
+    os.chdir(output_folder)
+    # subprocess.run(f'get_cds_alignments.py -f {input_folder}', shell=True)
+    
+    n_taxa = len([file for file in os.listdir(input_folder) if file.endswith('.fasta')])
+
+    os.chdir(udance_folder)
+    with open('config.yaml', 'r') as file:
+        data = yaml.safe_load(file)
+        data['workdir'] = output_folder
+        data['chartype'] = 'nuc'
+        data['backbone'] = 'de-novo'
+        data['resources']['cores'] = num_processes
+        data['mainlines_config']['n'] = n_taxa
+    with open('config1.yaml', 'w') as file:
+        yaml.safe_dump(data, file)
+
+    command = f'snakemake -c {num_processes} --configfile config1.yaml --snakefile udance.smk all'
+    execute_command_in_conda_env('udance', command)
+
+    # round2
+    subprocess.run(f'cp {output_folder}/output/udance.maxqs.nwk {output_folder}/backbone.nwk', shell=True)
+    subprocess.run(f'rm {output_folder}/udance.log', shell=True)
+    subprocess.run(f'rm -rf {output_folder}/output', shell=True)
 
 
-    if medaka_model:
-        command = f'medaka_consensus -i {input_reads} -d {output} -o medaka_result -m {medaka_model} -t {num_processes} > medaka.log'
-        conda_env = 'medaka'
-        execute_command_in_conda_env(conda_env, command)
-        subprocess.run(f'cp ./medaka_result/consensus.fasta {output}', shell=True)
-
-    if homopolish_model:
-        command = f'homopolish polish -a {output} -l {ref} -m {homopolish_model} -o homopolish-output'
-        conda_env = 'homopolish'
-        execute_command_in_conda_env(conda_env, command)
-        subprocess.run(f'cp ./homopolish-output/{strain}_homopolished.fasta {output}', shell=True)
+    data['backbone'] = 'tree'
+    with open('config2.yaml', 'w') as file:
+        yaml.safe_dump(data, file)
+    command = f'snakemake -c {num_processes} --configfile config2.yaml --snakefile udance.smk all'
+    execute_command_in_conda_env('udance', command)
+    
+    subprocess.run(f'mv {output_folder}/output {output_folder}/udance_output', shell=True)
+    subprocess.run(f'cp {output_folder}/output/udance.maxqs.nwk {output_folder}/tree.nwk', shell=True)
